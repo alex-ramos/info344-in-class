516322bb55edf16b5b999540dd40b3a4b4114295
alex-ramos <aramos3@uw.edu>
2017-03-29 14:58:36 -0700

    added zip functionality to server, added client page

--- zipsvr/main.go
+++ zipsvr/main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path"
+	"strings"
 )
+
+type zip struct {
+	Zip   string `json:"zip"`
+	City  string `json:"city"`
+	State string `json:"state"`
+}
+
+type zipSlice []*zip
+type zipIndex map[string]zipSlice
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
 
 	w.Header().Add("Content-Type", "text/plain")
+	w.Header().Add("Access-Control-Allow_Origin", "*")
 
 	w.Write([]byte("hello " + name))
+}
+
+func (zi zipIndex) zipsForCityHandler(w http.ResponseWriter, r *http.Request) {
+	// /zips/city/seattle
+	_, city := path.Split(r.URL.Path)
+	lcity := strings.ToLower(city)
+
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+
+	encoder := json.NewEncoder(w)
+	if err := encoder.Encode(zi[lcity]); err != nil {
+		http.Error(w, "error encoding json: "+err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
@@ -21,7 +47,31 @@ func main() {
 		log.Fatal("please set ADDR environment variable")
 	}
 
+	f, err := os.Open("../data/zips.json")
+	if err != nil {
+		log.Fatal("error opening zips file: " + err.Error())
+	}
+
+	zips := make([]*zip, 0, 42000)
+	decoder := json.NewDecoder(f)
+	if err := decoder.Decode(&zips); err != nil {
+		log.Fatal("error decoding zips file: " + err.Error())
+	}
+
+	fmt.Printf("loaded %d zips\n", len(zips))
+
+	zi := make(zipIndex)
+
+	for _, z := range zips {
+		lower := strings.ToLower(z.City)
+		zi[lower] = append(zi[lower], z)
+	}
+
+	fmt.Printf("there are %d zips in Seattle\n", len(zi["seattle"]))
+
 	http.HandleFunc("/hello", helloHandler)
+
+	http.HandleFunc("/zips/city/", zi.zipsForCityHandler)
 
 	fmt.Printf("server is listening at %s...\n", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
